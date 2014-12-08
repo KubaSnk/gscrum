128e32a792b4ee235f23ce06f1f30f8b0178a1b0
Jakub Wrabel <wrabel.jakub@gmail.com>
2014-12-08 19:39:34 +0100

    Commented out Java 1.8 code

---
+++ src/test/java/com/rwteam/gscrum/controller/googleapi/GoogleCalendarConnectorTest.java
@@ -0,0 +1,71 @@
+package com.rwteam.gscrum.controller.googleapi;
+
+import org.junit.Test;
+
+public class GoogleCalendarConnectorTest {
+    @Test
+    public void testConnect() {
+
+    }
+
+    @Test
+    public void testDisconnect() {
+
+    }
+
+    @Test
+    public void testGetStatus() {
+
+    }
+
+    @Test
+    public void testGetAllCalendars() {
+
+    }
+
+    @Test
+    public void testGetCalendar() {
+
+    }
+
+    @Test
+    public void testGetEvent() {
+
+    }
+
+    @Test
+    public void testGetCalendarEvents() {
+
+    }
+
+    @Test
+    public void testUpdateCalendar() {
+
+    }
+
+    @Test
+    public void testUpdateEvent() {
+
+    }
+
+    @Test
+    public void testAddCalendar() {
+
+    }
+
+    @Test
+    public void testAddEvent() {
+
+    }
+
+    @Test
+    public void testDeleteCalendar() {
+
+    }
+
+    @Test
+    public void testDeleteEvent() {
+
+    }
+
+}
